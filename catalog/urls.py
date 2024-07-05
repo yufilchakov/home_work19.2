d80a4994e9fddc063b08d8d42c0ca8f9c7be1d69
yura_filchakov <filchakov1993@gmail.com>
2024-07-06 04:14:46 +0700

    urls catalog

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts')
+]
