4a913a33c7fef2ec4efd6f3dcf3c0c2a2d0e191d
yura_filchakov <filchakov1993@gmail.com>
2024-07-25 02:32:16 +0700

    urls.py product

--- product/urls.py
+++ product/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = [
+from django.urls import path
+from product.apps import ProductConfig
+from product.views import product_list, product_detail
 
+app_name = ProductConfig.name
+
+urlpatterns = [
+    path('', product_list, name='product_list'),
+    path('product/<int:pk>/', product_detail, name='product_detail')
 ]
