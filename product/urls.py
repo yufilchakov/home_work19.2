a619771265f762e04eccc2a6bdf0427cf895f40e
yura_filchakov <filchakov1993@gmail.com>
2024-08-09 03:00:20 +0700

    urls.py 22.1

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from product.apps import ProductConfig
-from product.views import product_list, product_detail
+from product.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = ProductConfig.name
 
 urlpatterns = [
-    path('', product_list, name='product_list'),
-    path('product/<int:pk>/', product_detail, name='product_detail')
+    path('', ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
