94feed204f97c26c1222396125e8f04076934630
yura_filchakov <filchakov1993@gmail.com>
2024-08-09 02:57:28 +0700

    admin.py 22.1

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from product.models import Category, Product
+from product.models import Category, Product, Version
 
 
 @admin.register(Category)
@@ -12,3 +12,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'description', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+    
+    
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'version_number', 'version_name', 'current_version_indicator',)
