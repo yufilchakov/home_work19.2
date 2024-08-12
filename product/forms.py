c0c38ca284d22b5e6d9b6a32ae6a951630ed3f9f
yura_filchakov <filchakov1993@gmail.com>
2024-08-13 03:24:04 +0700

    forms.py 22.1

--- product/forms.py
+++ product/forms.py
@@ -1,5 +1,4 @@
-from django.forms import ModelForm, BooleanField, forms
-from django.core.exceptions import ValidationError
+from django.forms import ModelForm, BooleanField, forms, BaseInlineFormSet
 
 from product.models import Product, Version
 
@@ -12,34 +11,49 @@ class StyleFormMixin:
                 fild.widget.attrs['class'] = 'form-check-input'
             else:
                 fild.widget.attrs['class'] = 'form-control'
-                
+
 
 class ProductForm(StyleFormMixin, ModelForm):
     """Класс форма для продуктов"""
+    
     class Meta:
         model = Product
         exclude = ('views',)
-
-        
-class VersionForm(StyleFormMixin, ModelForm):
-    """Класс форма для версий"""
-    class Meta:
-        model = Version
-        fields = '__all__'
-        
+    
+    forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                       'радар']
+    
     def clean_name(self):
         cleaned_data = self.cleaned_data['name']
-        forbidden_words = ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
-                           'радар')
-        if cleaned_data in forbidden_words:
-            raise forms.ValidationError('Название не должно содержать запрещенные слова')
+        for forbidden_word in self.forbidden_words:
+            if forbidden_word in cleaned_data.lower():
+                raise forms.ValidationError(f'Название не должно содержать запрещенные слова: {forbidden_word}')
         return cleaned_data
     
     def clean_description(self):
         cleaned_data = self.cleaned_data['description']
-        forbidden_words = ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
-                           'радар')
-        if cleaned_data in forbidden_words:
-            raise forms.ValidationError('Описание не должно содержать запрещенные слова')
+        for forbidden_word in self.forbidden_words:
+            if forbidden_word in cleaned_data.lower():
+                raise forms.ValidationError(f'Описание не должно содержать запрещенные слова: {forbidden_word}')
         return cleaned_data
+
+
+class VersionForm(StyleFormMixin, ModelForm):
+    """Класс форма для версий"""
+    
+    class Meta:
+        model = Version
+        fields = '__all__'
         
+        
+class VersionFormset(StyleFormMixin, BaseInlineFormSet):
+    def clean_count(self):
+        super().clean()
+        count = 0
+        for form in self.forms:
+            if form.instance.current_version_indicator:
+                count += 1
+        if count > 1:
+            raise forms.ValidationError("Может быть только 1 активная версия")
+        return count
+    
