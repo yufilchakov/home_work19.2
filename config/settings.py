901c334ab687ec3acaa9e115360721732d6599d8
yura_filchakov <filchakov1993@gmail.com>
2024-08-19 01:30:29 +0700

    settings.py 22.2

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'catalog',
     'product',
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "yura-filchakov1993@yandex.ru"
+EMAIL_HOST_PASSWORD = "rpwdoqzzpevzpnku"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
